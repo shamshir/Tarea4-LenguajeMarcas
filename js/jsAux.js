bf4889eff11acd1d020f49ad1ecab5d9ed6be274
shamshir <javi_vavi@hotmail.com>
2018-05-31 22:28:48 +0200

    Update jsAux.js

--- js/jsAux.js
+++ js/jsAux.js
@@ -4,16 +4,16 @@ $(document).ready(function(){
 	
 	offset = $("#barraNav").offset();
 	
-});
+	$(window).scroll(function(){
 
-$(window).scroll(function(){
-	
-	if (window.pageYOffset >= offset.top) {
-		$("#barraNav").addClass("sticky");
-		$("#principal").css("padding-top","70px");
-	} else {
-		$("#barraNav").removeClass("sticky");
-		$("#principal").css("padding-top","0px");
-	}
+		if (window.pageYOffset >= offset.top) {
+			$("#barraNav").addClass("sticky");
+			$("#principal").css("padding-top","70px");
+		} else {
+			$("#barraNav").removeClass("sticky");
+			$("#principal").css("padding-top","0px");
+		}
+
+	});
 	
 });
